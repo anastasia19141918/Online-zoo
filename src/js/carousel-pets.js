428537ffbd04c73e4d2c2a15ae06e8b0cc7a08b4
Anastasiya <anastasiya.anast.ru@mail.ru>
2024-12-24 18:53:05 +0300

    fix: add slaider

--- src/js/carousel-pets.js
+++ src/js/carousel-pets.js
@@ -3,8 +3,12 @@ import shuffle from './random.js';
 import creatItemns from './creat.js';
 
 const galleryItemns = document.getElementById('carousel__itemns');
+const itemns = document.querySelectorAll('.gallery__itemns');
 const left = document.getElementById('btn-left');
 const right = document.getElementById('btn-right');
+
+let isEnabled = true;
+let currentItemn = 0;
 
 const galleryItemnsFirst = document.getElementById('gallery__itemns_first');
 const galleryItemnSecond = document.getElementById('gallery__itemns_second');
@@ -14,24 +18,27 @@ left.addEventListener('click', moveLeft);
 right.addEventListener('click', moveRight);
 galleryItemns.addEventListener('animationend', slaider);
 
+
 function moveLeft () {
   galleryItemns.classList.add('transition-left');
+  galleryItemnsFirst.innerHTML = '';
+  newArr(data);
+  galleryItemnSecond.innerHTML = galleryItemnsFirst.innerHTML;
 };
 
 function moveRight () {
   galleryItemns.classList.add('transition-right');
+  galleryItemnSecond.innerHTML = galleryItemnsFirst.innerHTML;
+  galleryItemnsFirst.innerHTML = '';
+  newArr(data);
 };
 
 function slaider (animationEvent) {
   if (animationEvent.animationName === 'move-left') {
     galleryItemns.classList.remove('transition-left');
-    galleryItemnsBlock.innerHTML = '';
-    newArr(data);
   };
   if (animationEvent.animationName === 'move-right') {
     galleryItemns.classList.remove('transition-right');
-    galleryItemnsBlock.innerHTML = '';
-    newArr(data);
   };
 };
 
@@ -39,10 +46,7 @@ function newArr(data) {
   shuffle (data);
   let newItemns = data.slice(9);
   creatItemns (newItemns);
-  
 };
-
-
 
 
 
